7defdf1e717ae065f19cdf0c5d847f438ef033a5
Natalia Dolgushina <paninebaluy@gmail.com>
2020-03-31 01:15:52 +0300

    completed additional routes and error handling

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,15 @@
 const router = require('express').Router({ mergeParams: true });
 
-const { getAllCards, postCard, deleteCard } = require('../controllers/cards');
+const {
+  getAllCards, postCard, deleteCard, likeCard, dislikeCard,
+} = require('../controllers/cards');
 const { cardErrorHandler } = require('../middleware/errorHandler');
 
 router.get('/', getAllCards);
 router.post('/', postCard);
 router.delete('/:id', deleteCard);
+router.put('/:id/likes', likeCard);
+router.delete('/:id/likes', dislikeCard);
 // an error handler is the last middleware:
 router.use(cardErrorHandler);
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 const router = require('express').Router({ mergeParams: true });
-const { userErrorHandler } = require('../middleware/errorHandler');
 
-const { getAllUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getAllUsers, getUser, createUser, updateUserProfile, updateUserAvatar,
+} = require('../controllers/users');
+const { userErrorHandler } = require('../middleware/errorHandler');
 
 router.get('/:id', getUser);
 router.get('/', getAllUsers);
 router.post('/', createUser);
+router.patch('/me', updateUserProfile);
+router.patch('/me/avatar', updateUserAvatar);
 // an error handler is the last middleware:
 router.use(userErrorHandler);
 
