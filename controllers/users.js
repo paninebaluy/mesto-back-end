de51a987e983a974697d196aef1e099bc4d68f65
Natalia Dolgushina <paninebaluy@gmail.com>
2020-03-31 01:15:52 +0300

    completed additional routes and error handling

--- controllers/users.js
+++ controllers/users.js
@@ -24,12 +24,39 @@ const createUser = (req, res, next) => {
 };
 
 // PATCH /users/me — обновляет профиль
+const updateUserProfile = (req, res, next) => {
+  const { name, about, avatar } = req.body;
 
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    .then((user) => {
+      if (name || about || avatar) {
+        res.status(200, 'OK').json({ data: user });
+      } else {
+        res.status(400, 'Bad Request').json({ message: 'Not a valid property' }); // этот обработчик работает только здесь :(
+      }
+    })
+    .catch(next); // passes the data to error handler
+};
 
 // PATCH /users/me/avatar — обновляет аватар
+const updateUserAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    .then((user) => {
+      if (avatar) {
+        res.status(200, 'OK').json({ data: user });
+      } else {
+        res.status(400, 'Bad Request').json({ message: 'Not a valid property' }); // этот обработчик работает только здесь :(
+      }
+    })
+    .catch(next); // passes the data to error handler
+};
 
 module.exports = {
   getAllUsers,
   getUser,
   createUser,
+  updateUserProfile,
+  updateUserAvatar,
 };
