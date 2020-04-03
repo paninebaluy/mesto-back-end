aba85477cf7727cd71c863efd21a6bdf0ce68e9b
Natalia Dolgushina <paninebaluy@gmail.com>
2020-04-03 13:40:46 +0300

    minor fixes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-const errorHandler = (error, req, res, next) => res.status(error.status || 500).send({
-  error: {
-    status: error.status,
-    message: error.message,
-  },
-});
+const errorHandler = (error, req, res, next) => {
+  const status = error.status || 500;
+  const message = status === 500 ? 'Internal Server Error' : error.message;
+  res.status(status).send({
+    error: {
+      status,
+      message,
+    },
+  });
+};
 
 module.exports = errorHandler;
