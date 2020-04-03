5abdf2660512751d83020d9341d7089cc3a003a3
Natalia Dolgushina <paninebaluy@gmail.com>
2020-04-03 13:40:46 +0300

    minor fixes

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,5 @@
-const mongoose = require('mongoose');
-
 const User = require('../models/user');
 const NotFoundError = require('../errors/notFoundError');
-const InternalServerError = require('../errors/internalServerError');
 
 // GET /users — возвращает всех пользователей
 const getAllUsers = (async (req, res, next) => {
@@ -19,19 +16,19 @@ const getAllUsers = (async (req, res, next) => {
 const getUser = (async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
+    if (!user) {
+      throw new NotFoundError('Not Found');
+    }
     res.status(200).send({ data: user });
   } catch (err) {
-    if (err instanceof mongoose.CastError) {
-      next(new NotFoundError('Not Found'));
-    }
     next(err); // passes the data to error handler
   }
 });
 
 // POST /users — создаёт пользователя
 const createUser = (async (req, res, next) => {
-  const { name, about, avatar } = req.body;
   try {
+    const { name, about, avatar } = req.body;
     const user = await User.create({ name, about, avatar });
     res.status(201).send({ data: user });
   } catch (err) {
@@ -41,11 +38,8 @@ const createUser = (async (req, res, next) => {
 
 // PATCH /users/me — обновляет профиль
 const updateUserProfile = (async (req, res, next) => {
-  const { name, about } = req.body;
   try {
-    if (!(name || about)) {
-      throw new InternalServerError('One or more of fields required: name, about');
-    }
+    const { name, about } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { name, about },
       { new: true, runValidators: true });
     res.status(200).send({ data: user });
@@ -56,11 +50,8 @@ const updateUserProfile = (async (req, res, next) => {
 
 // PATCH /users/me/avatar — обновляет аватар
 const updateUserAvatar = (async (req, res, next) => {
-  const { avatar } = req.body;
   try {
-    if (!avatar) {
-      throw new InternalServerError('One or more of fields required: avatar');
-    }
+    const { avatar } = req.body;
     const user = await User.findByIdAndUpdate(req.user._id, { avatar },
       { new: true, runValidators: true });
     res.status(200).send({ data: user });
--- routes/invalidRoutes.js
+++ routes/invalidRoutes.js
@@ -2,8 +2,8 @@ const invalidUrl = require('express').Router({ mergeParams: true });
 const NotFoundError = require('../errors/notFoundError');
 
 // eslint-disable-next-line no-unused-vars
-invalidUrl.all('*', (req, res) => {
-  throw new NotFoundError('Not Found');
+invalidUrl.all('*', (req, res, next) => {
+  next(new NotFoundError('Not Found'));
 });
 
 module.exports = invalidUrl;
