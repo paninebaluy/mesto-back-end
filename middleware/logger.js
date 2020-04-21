e34bc125effddea081d189f5510d8e4ca140335d
Natalia Dolgushina <paninebaluy@gmail.com>
2020-04-22 00:36:20 +0300

    configured logging with winston

--- middleware/logger.js
+++ middleware/logger.js
@@ -5,10 +5,17 @@ const expressWinston = require('express-winston');
 const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({
+      level: 'info',
       filename: 'request.log',
+      handleExceptions: true,
+      json: true,
+      maxsize: 1048576, // 1MB
+      colorize: true,
     }),
   ],
   format: winston.format.json(),
+  level: 'info',
+  exitOnError: false,
 });
 
 // logger of errors
@@ -16,8 +23,16 @@ const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({
       filename: 'error.log',
+      level: 'error, crit',
+      handleExceptions: true,
+      json: true,
+      maxsize: 1048576, // 1MB
+      colorize: true,
     }),
   ],
+  format: winston.format.json(),
+  level: 'error, crit',
+  exitOnError: false,
 });
 
 module.exports = {
