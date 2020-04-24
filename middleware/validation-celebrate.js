e1d7aabda1c72614b7fcb6505a57ba47f05e676a
Natalia Dolgushina <paninebaluy@gmail.com>
2020-04-24 23:22:49 +0300

    removed node_modules dir

--- middleware/validation-celebrate.js
+++ middleware/validation-celebrate.js
@@ -7,7 +7,7 @@ const errors = {
   name: new BadRequestError('Name must contain 2 to 30 symbols'),
   avatar: new BadRequestError('Field "avatar" must be a valid URL'),
   email: new BadRequestError('Email is required'),
-  password: new BadRequestError('Password must contain at least 8 characters'),
+  password: new BadRequestError('Password must contain 8 to 24 characters. Letters from a to z, numbers and special chars are allowed.'),
   link: new BadRequestError('Link must be a valid URL'),
   id: new BadRequestError('Must be a Mongoose ObjectID'),
   login: new UnauthorizedError('The email or password you entered are not valid'),
@@ -20,11 +20,12 @@ const userValidator = celebrate({
     about: Joi.string().required().min(2).max(30)
       .error(errors.about),
     avatar: Joi.string().required()
-      .regex(/^(http|https):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-]*)?#?$/)
+      .regex(/^(https?):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-_.#:?&~/]*)?$/)
       .error(errors.avatar),
     email: Joi.string().required().email()
       .error(errors.email),
     password: Joi.string().required().min(8)
+      .regex(/[a-z0-9-+_=&?!.,%^:;<>#@*()~'"|\\/]{8,24}/i)
       .error(errors.password),
   }),
 });
@@ -51,7 +52,7 @@ const profileUpdateValidator = celebrate({
 const avatarUpdateValidator = celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required()
-      .regex(/^(http|https):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-]*)?#?$/)
+      .regex(/^(https?):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-_.#:?&~/]*)?$/)
       .error(errors.avatar),
   }),
 });
@@ -61,7 +62,8 @@ const cardValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30)
       .error(errors.name),
-    link: Joi.string().required().regex(/^(http|https):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-]*)?#?$/)
+    link: Joi.string().required()
+      .regex(/^(https?):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z1-9_-]+\.[А-ЯЁа-яёA-Za-z1-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z1-9/-_.#:?&~/]*)?$/)
       .error(errors.link),
   }),
 });
